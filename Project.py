5b180bc6d0dc14b95dc7e5527bccd9793d896bfd
gulu864 <suva.kanta4u@gmail.com>
2024-11-09 18:38:11 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.Screen().bgcolor("Yellow")
+turtle.Screen().setup(300,400)
+var1=turtle.Turtle()
+
+side_length=70
+angle=360
+
+for i in range(0,4,1):
+ var1.forward(100)
+ var1.right(90)
+
+turtle.done()
